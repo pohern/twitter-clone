6eacc124ee0fe623fcfc4f98da59d1f18454f4a2
pohern <ohern_p@yahoo.com>
2021-06-08 09:32:10 -0500

    Create signup page and verify that the token is being passed correctly

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,32 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import React from 'react';
-import './App.css';
-import Users from './components/Users';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Landing from './components/Landing';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import React from "react";
+import "./App.css";
+import Users from "./components/Users";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./components/Landing";
+import { setContext } from "apollo-link-context";
+import SignUp from "./pages/SignUp";
 
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+const authLink = setContext(async (req, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const link = authLink.concat(httpLink as any)
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: (link as any),
   cache: new InMemoryCache()
 })
 
@@ -17,6 +37,9 @@ function App() {
         <Switch>
           <Route path='/landing'>
             <Landing />
+          </Route>
+          <Route path='/signup'>
+            <SignUp />
           </Route>
           <Route path='/'>
             <Users />
