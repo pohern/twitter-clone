deaccb1d5ff6ab58c15e231eb482c77f8826a438
pohern <ohern_p@yahoo.com>
2021-06-08 08:50:05 -0500

    Restructure file for GitHub

---
+++ web/src/App.tsx
@@ -0,0 +1,30 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React from 'react';
+import './App.css';
+import Users from './components/Users';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Landing from './components/Landing';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache()
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route path='/landing'>
+            <Landing />
+          </Route>
+          <Route path='/'>
+            <Users />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
