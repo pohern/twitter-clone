53f04cde586ff13b5dffad7df88da95bb208d73b
pohern <ohern_p@yahoo.com>
2021-06-09 06:17:47 -0500

    Issue discovered in profile, working out bug now

--- web/src/pages/Profile.tsx
+++ web/src/pages/Profile.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+export const ME_QUERY = gql`
+  query me {
+    me {
+      id
+      Profile {
+        id
+        bio
+        location
+        website
+        avatar
+      }
+    }
+  }
+`;
 
 function Profile() {
-    return (
-        <div>
-            <h1>Profile</h1>
-            
-        </div>
-    )
+    const { loading, error, data } = useQuery(ME_QUERY)
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>{error.message}</p>
+  return (
+    <div className='container'>
+      <h1>Profile</h1>
+      {/* {data.me.Profile.id ? } */}
+      {/* <p>{data.me.profile.bio}</p>
+      <p>{data.me.profile.location}</p>
+      <p>{data.me.profile.website}</p> */}
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
